145733bafa20e8dc68628f4495e8a93b06819863
Golegarios Chagas <lucas.chagasbom570@gmail.com>
2023-05-07 18:47:39 -0300

    v1.2
    1. Adicionei intents
    2. Corrigi o erro do command
    3. Corrigi o erro gramatical kkkkk

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='?')
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='?', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'Logado em {bot.user.name}')
 
-@bot.command
+@bot.command()
 async def oi(mensagem):
     await mensagem.send('Olá!')
 
 #Acontece que na primeira versão eu esqueci de por a função token kkkkkkk
-bot.run('Digite seu toke aqui!')
+bot.run('Digite aqui seu token')
