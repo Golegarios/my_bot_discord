579606faff5a0e66d86c532a9cb63d2ecae0da5f
Golegarios Chagas <105462052+Golegarios@users.noreply.github.com>
2023-05-09 11:53:39 -0300

    v1.4
    1. Função passiva prefixo no nome
    2. Função passiva estar jogando algo
    3. Corrigi algumas coisas
    4. Prefixo ganhou uma variável

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from discord.ext import commands
 #Compreendo que foi falta de profissionalismo meu nos codigos ou versões anteriores de não verificar antes de enviar.
 #Deixando claro que sou um desenvolvedor Brasileiro e que "kkkkk" independe da quantidade aqui significa risada.
 
+prefix = '?'
 intents = discord.Intents().all()
-bot = commands.Bot(command_prefix='?', intents=intents)
-# Esse aqui apenas mostra informações no console
+bot = commands.Bot(command_prefix=prefix, intents=intents)
+
+# Funções Passivas
 @bot.event
 async def on_ready():
-    print(f'Logado em {bot.user.name}')
+    guild = bot.guilds[0]
+    nick_name = bot.user.name
+    member = guild.get_member(bot.user.id)
+    print(f'Logado em {nick_name}') # Diz que está logado no console
+    await bot.change_presence(activity=discord.Game("exemplo de jogo")) # Aqui diz oq ele esta jogando
+    await member.edit(nick=f'[{prefix}]{nick_name}') # Nome fixo do bot com prefixo desde o inicio
 
 # Daqui em diante é apenas comando, estou pensando em por um sistema de classes
 @bot.command()
@@ -17,4 +24,4 @@ async def oi(msg):
     await msg.send('Olá!')
     
 # Aqui é só o Token
-bot.run('Digite aqui seu token')
+bot.run('Digite seu token')
